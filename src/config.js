6bf263c28ddc55a13a2e4a5b4eb6ddc5b4da7132
Henrry Mojica <hleomojica@gmail.com>
2021-12-27 14:29:59 +1100

    public files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "";
+const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "back.opem.com.co";
 const portApi = process.env.NODE_ENV === "development" ? 3000 : "";
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api/v1`;
 const redirectUrl = process.env.NODE_ENV === "http://localhost:3000/verify-email"
